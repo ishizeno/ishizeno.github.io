6ce257bcd3c7a647f8215d000d919b4df1402157
Ismael Ghalimi <ismael@sutoiku.com>
2015-02-03 23:24:47 +0100

    Updated Home Page

--- js/custom.js
+++ js/custom.js
@@ -106,6 +106,20 @@ $(document).ready(function() {
         autoPlay: 10000
     });
 
+    $("#details-slide").owlCarousel({
+        // Most important owl features
+        items: 1,
+        itemsCustom: false,
+        itemsDesktop: [1199, 1],
+        itemsDesktopSmall: [980, 1],
+        itemsTablet: [768, 1],
+        itemsTabletSmall: false,
+        itemsMobile: [479, 1],
+        singleItem: false,
+        startDragging: true,
+        autoPlay: 10000
+    });
+
 });
 
 /*=========================*/
